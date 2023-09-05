9fc70f4c7acd480fb5d5d127c75a21a8ec3c5917
Guilherme <devguicordeiro@gmail.com>
2023-09-05 10:25:50 -0300

    madlibs working properly

--- MADLIBS/madlibs.py
+++ MADLIBS/madlibs.py
@@ -1,5 +1,16 @@
-player = "lepão"
+# player = "lepão"
 
-print("pega sejuani " + player)
-print("pega sejuani {}".format(player))
-print(f"pega sejuani {player}")
+# print("pega sejuani " + player)
+# print("pega sejuani {}".format(player))
+# print(f"pega sejuani {player}")
+
+adj = input("Adjective: ")
+verbOne = input("VerbOne: ")
+verbTwo = input("VerbTwo: ")
+famousPerson = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited \
+    all the time because I love to {verbOne}. Stay hydrated and {verbTwo} \
+    like you are {famousPerson}"
+
+print(madlib)
