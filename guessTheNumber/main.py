9f92d5361d03fe7dec37b161c674e2f1157c3454
Guilherme <devguicordeiro@gmail.com>
2023-09-06 21:45:37 -0300

    added whole function of new guessing

--- guessTheNumber/main.py
+++ guessTheNumber/main.py
@@ -19,6 +19,16 @@ def computerGuess(x):
     low = 1
     high = x
     feedback = ""
+    while feedback != "c" and low != high:
+        guess = random.randint(low, high)
+        feedback = input(
+            f"Is {guess} too high (H), too low (L), or correct (C)? ").lower()
+        if feedback == "h":
+            high = guess - 1
+        elif feedback == "l":
+            low == guess + 1
+
+    print(f"Yay! The bot correctly guessed your number, {guess}!")
 
 
-guess(10)
+computerGuess(10)
