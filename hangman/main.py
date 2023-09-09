ba2a25f1b5f91eace9f79f972ebd48425bbcff42
Guilherme <devguicordeiro@gmail.com>
2023-09-09 20:34:10 -0300

    app working properly

--- hangman/main.py
+++ hangman/main.py
@@ -12,23 +12,34 @@ def get_valid_word(words):
 
 
 def hangman():
-    word = get_valid_wor(words)
+    word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    user_letter = input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    # userinput
 
-    elif user_letter in used_letters:
-        print("You have already used that letter. Please try another one.")
+    while len(word_letters) > 0:
 
-    else:
-        print("Invalid letter. Try again.")
+        # letter used
+        print("You have used these letters: ", " ".join(used_letters))
+
+        # current word dashes
+        word_list = [
+            letter if letter in used_letters else "-" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print("You have already used that letter. Please try another one.")
+
+        else:
+            print("Invalid letter. Try again.")
 
 
-user_input = input("Type a letter: ")
-print(user_input)
+hangman()
