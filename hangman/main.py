ca0bf29fa934d5b3e0d7077db3092bc58e366f52
Guilherme <devguicordeiro@gmail.com>
2023-09-09 20:05:13 -0300

    added main functions

--- hangman/main.py
+++ hangman/main.py
@@ -1,4 +1,5 @@
 import random
+import string
 from words import words
 
 
@@ -8,3 +9,26 @@ def get_valid_word(words):
         word = random.choice(words)
 
     return word.upper()
+
+
+def hangman():
+    word = get_valid_wor(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+    elif user_letter in used_letters:
+        print("You have already used that letter. Please try another one.")
+
+    else:
+        print("Invalid letter. Try again.")
+
+
+user_input = input("Type a letter: ")
+print(user_input)
