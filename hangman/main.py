584efa4b9d5471ed6ad5f0e0890688d6ec2f4024
Guilherme <devguicordeiro@gmail.com>
2023-09-10 20:44:19 -0300

    adding the concept of lives

--- hangman/main.py
+++ hangman/main.py
@@ -16,13 +16,15 @@ def hangman():
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
+    lives = 5
 
     # userinput
 
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
 
         # letter used
-        print("You have used these letters: ", " ".join(used_letters))
+        print("You have", lives, "lives left and you have used these letters: ",
+              " ".join(used_letters))
 
         # current word dashes
         word_list = [
@@ -34,6 +36,10 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+
+            else:
+                lives = lives - 1
+                print("Letter is not in the word.")
 
         elif user_letter in used_letters:
             print("You have already used that letter. Please try another one.")
