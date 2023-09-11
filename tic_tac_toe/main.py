1597d9a9847359cd8a4651e1ba5b6bfd1bcb7ca4
Guilherme <devguicordeiro@gmail.com>
2023-09-11 13:30:42 -0300

    added board print

---
+++ tic_tac_toe/main.py
@@ -0,0 +1,16 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _ in range(9)]  # list to represent board
+        self.current_winner = None  # track of the winner
+
+    def print_board(self):
+        # settings the rows
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print("| " + " | ".join(row) + " |")
+
+    @staticmethod
+    def print_board_nums():
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)]
+                        for j in range(3)]
+        for row in number_board:
+            print("| " + " | ".join(row) + " |")
