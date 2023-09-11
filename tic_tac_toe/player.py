538439015aba7dd13128f32158a50c932bd88e63
Guilherme <devguicordeiro@gmail.com>
2023-09-11 19:31:45 -0300

    created get moves functions

--- tic_tac_toe/player.py
+++ tic_tac_toe/player.py
@@ -15,7 +15,8 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 
 class HumanPlayer(Player):
@@ -23,4 +24,16 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + "'s turn. Input move (0-9:): ")
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print("Invalid square. Try again.")
+
+        return val
