853af99d1d20451297b1d19608a4d6713f584137
Guilherme <devguicordeiro@gmail.com>
2023-09-10 21:19:13 -0300

    init tic-tac-toe and readme

---
+++ tic_tac_toe/README.md
@@ -0,0 +1,10 @@
+# TIC-TAC-TOE APP
+
+Just a quick tic-tac-toe
+</br>
+Notes: </br>
+I'm still trying to get used to all the programming language changes, but things is working pretty well.
+</br>
+</br>
+I'm commiting these so I'm aware that I was not doing
+nothing in relation to learning how to code. I'm taking the long road since I need the short now, it results in having no time at all, but the  small amout of time I have will be making this effort.
---
+++ tic_tac_toe/player.py
@@ -0,0 +1,26 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        pass
