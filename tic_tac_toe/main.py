6d0e3ea996ac2cbd32c1f3d617ec41f6e93ae48d
Guilherme <devguicordeiro@gmail.com>
2023-09-11 21:16:05 -0300

    added winners check

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -34,7 +34,31 @@ class TicTacToe:
     def make_move(self, square, letter):
         if self.board[square] == " ":
             self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
             return True
+        return False
+
+    def winner(self, square, letter):
+        # checks winner
+        row_ind = square // 3
+        row = self.board[row_ind*3: (row_ind + 1) * 3]
+        if all([spot == letter for spot in row]):
+            return True
+
+        col_ind = square % 3
+        column = [self.board[col_ind+i*3] for i in range(3)]
+        if all([spot == letter for spot in column]):
+            return True
+
+        if square % 2 == 0:
+            diagonal1 = [self.board[i] for i in [0, 4, 8]]
+            if all([spot == letter for spot in diagonal1]):
+                return True
+            diagonal2 = [self.board[i] for i in [2, 4, 6]]
+            if all([spot == letter for spot in diagonal2]):
+                return True
+
         return False
 
 
@@ -56,4 +80,12 @@ def play(game, x_player, o_player, print_game=True):
                 game.print_board()
                 print("")
 
+            if game.current_winner:
+                if print_game:
+                    print(letter + " wins!!")
+                return letter
+
             letter = "O" if letter == "X" else "X"
+
+        if print_game:
+            print("It's a tie!")
