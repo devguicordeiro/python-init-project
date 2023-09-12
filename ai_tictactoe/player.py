d8f04c65b72268450a1452a556b20b5d59f4823a
Guilherme <devguicordeiro@gmail.com>
2023-09-12 19:31:13 -0300

    added AI functions

--- ai_tictactoe/player.py
+++ ai_tictactoe/player.py
@@ -37,3 +37,39 @@ class HumanPlayer(Player):
                 print("Invalid square. Try again.")
 
         return val
+
+
+class AiComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            square = self.minimax(game, self.letter)
+        return square
+
+    def minima(self, state, player):
+        max_player = self.letter
+        other_player = "O" if player == "X" else "X"
+
+        if state.current_winner == other_player:
+            return {"position": None,
+                    "score": 1 * (state.num_empty_square() + 1) if other_player == max_player
+                    else -1 * (state.num_empty_square() + 1)
+                    }
+
+        elif not state.empty_squares():
+            return {"position": None,
+                    "score": 0
+                    }
+
+        if player == max_player:
+            best = {"position": None,
+                    "score": -math.inf
+                    }
+        else:
+            best = {"position": None,
+                    "score": math.inf
+                    }
