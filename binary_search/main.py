3a75a7a200568a42580ee6725da8ae73a3bd2fc6
Guilherme <devguicordeiro@gmail.com>
2023-09-19 19:41:42 -0300

    both naive and binary code

--- binary_search/main.py
+++ binary_search/main.py
@@ -5,12 +5,21 @@ def naive_search(l, target):
     return -1
 
 
-def binary_search(l, target):
-    midpoint = (len(l)) // 2
+def binary_search(l, target, low=None, high=None):
+    if low is None:
+        low = 0
+
+    if high is None:
+        high = len(l) - 1
+
+    if high < low:
+        return -1
+
+    midpoint = (low + high) // 2
 
     if l[midpoint] == target:
         return midpoint
     elif target < l[midpoint]:
-        return binary_search(l, target)
+        return binary_search(l, target, low, midpoint-1)
     else:
-        return binary_search(l, target)
+        return binary_search(l, target, midpoint+1, high)
