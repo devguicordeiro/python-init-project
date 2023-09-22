8e719fdde53d2362961d2ce8c941c16c07fa7a18
Guilherme <devguicordeiro@gmail.com>
2023-09-21 21:07:30 -0300

    starting board creation

---
+++ minesweeper/main.py
@@ -0,0 +1,10 @@
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        self.dug = set()
+
+
+def play(dim_size=10, num_bombs=10):
+    pass
